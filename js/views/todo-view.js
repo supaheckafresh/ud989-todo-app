86d2c1d2a8b90d548711b0bb26e2528bef507d0f
supaheckafresh <neilstrain@gmail.com>
2016-02-11 11:35:44 -0800

    Add priority button and wire up to make li's red when selected

--- js/views/todo-view.js
+++ js/views/todo-view.js
@@ -17,6 +17,7 @@ var app = app || {};
 
 		// The DOM events specific to an item.
 		events: {
+			'click .priority-btn': 'togglePriority',
 			'click .toggle': 'toggleCompleted',
 			'dblclick label': 'edit',
 			'click .edit-btn': 'edit',
@@ -50,6 +51,7 @@ var app = app || {};
 			}
 
 			this.$el.html(this.template(this.model.toJSON()));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.$el.toggleClass('completed', this.model.get('completed'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
@@ -64,6 +66,11 @@ var app = app || {};
 			return this.model.get('completed') ?
 				app.TodoFilter === 'active' :
 				app.TodoFilter === 'completed';
+		},
+
+		// Toggle the "priority" state of the model
+		togglePriority: function () {
+			this.model.togglePriority();
 		},
 
 		// Toggle the `"completed"` state of the model.
